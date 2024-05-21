4ec0bb413bf8803252209d5f44ed27402d23d764
DavidEspinoza677 <2005espinozadvid@gmail.com>
2024-05-21 14:37:32 -0600

    principal

---
+++ SesionMayo21/functions.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include "variables.h"
+using namespace std;
+
+/*
+C CREATE
+R READ
+U UPDATE
+D DELETE
+*/
+
+// create
+void addReg();
+void showReg();
+void menu();
+{
+    cout << "Datos del estudiante\n";
+    cout << "CIF: ";
+    scanf(" %[^\n]", people[pos].cif);
+    cout << "NOMBRE: ";
+    scanf(" %[^\n]", people[pos].name);
+    cout << "APELLIDO: ";
+    scanf(" %[^\n]", people[pos].lastname);
+    cout << "AÑO: ";
+    scanf("%d", &people[pos].year);
+}
+
+void showReg();
+void showReg()
+{
+    for (int i = 0; i < pos; i++)
+    {
+        printf("Nombre: %s %s \nAño: %d", people[i].name, people[i].lastname, people[i].year);
+        cout << "*************************\n";
+    }
+}
+
+void menu()
+{
+    int op = 0;
+    do
+    { systeam("cls || clear")
+        cout << "1. Agregar \n";
+        cout << "2. Mostrar \n";
+        cout << "3. Salir \n";
+        cout << "Opcion: ";
+        cin >> op;
+        switch (op)
+        {
+        case 1:
+            addReg();
+            system("pause");
+            break;
+        case 2:
+            showReg();
+            system("pause");
+            break;
+        case 3:
+            cout << "Adios" << endl;
+            break;
+        default:
+            break;
+        }
+
+    } while (op != 3);
+}
--- SesionMayo21/principal.cpp
+++ SesionMayo21/principal.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
-// #include "variables.h"
+#include "functions.cpp"
 using namespace std;
-
-struct
-{
-    char cif[60];
-    char name[60];
-    char lastname[60];
-    int year;
-} person;
 
 int main(int argc, char const *argv[])
 {
-    /* code */
-    cout << "CIF: ";
-    cin >> person.cif;
-    cout << "Nombre: ";
-    cin >> person.name;
+    menu();
 
-    cout << person.cif << " - " << person.name << endl;
     return 0;
 };
